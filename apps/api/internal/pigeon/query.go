5735b309414c8e0d6c7e6a5b1d934416550ee5f6
Reiniel Fernandez Cruz <git@reinielfc.dev>
2026-07-25 12:59:58 -0400

    refactor(pigeon): reorder query service and summary types

--- apps/api/internal/pigeon/query.go
+++ apps/api/internal/pigeon/query.go
@@ -7,6 +7,21 @@ import (
 	"github.com/google/uuid"
 )
 
+type QueryService interface {
+	// List retrieves a list of all pigeons in the database.
+	//
+	// Returns a slice of [Summary] and the total count of pigeons matching the filter criteria.
+	List(ctx context.Context, f ListFilter) ([]Summary, int, error)
+}
+
+// ListFilter represents the filtering options for listing pigeons, including pagination parameters.
+type ListFilter struct {
+	Page  int
+	Limit int
+}
+
+// Summary represents a summary of a pigeon entity, including its ID, name, ring number, etc.
+// This struct is used for listing pigeons with essential information.
 type Summary struct {
 	ID            uuid.UUID
 	Name          *string
@@ -17,16 +32,4 @@ type Summary struct {
 	AcquiredDate  *time.Time
 	AcquiredVia   AcquisitionMethod
 	CreatedAt     time.Time
-}
-
-type ListFilter struct {
-	Page  int
-	Limit int
-}
-
-type QueryService interface {
-	// List retrieves a list of all pigeons in the database.
-	//
-	// Returns a slice of [Summary] and the total count of pigeons matching the filter criteria.
-	List(ctx context.Context, f ListFilter) ([]Summary, int, error)
 }
